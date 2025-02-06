097953e2de2b42dbeafa86953351ad3711d54f3a
toms412 <thomas.mazeau@icloud.com>
2025-02-07 00:41:34 +0100

    Add: Home page setup

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,13 +1,46 @@
-import React from 'react'
+"use client"
+import MeetingTypeList from '@/components/MeetingTypeList';
+import React, { useEffect, useState } from 'react';
 
 const Home = () => {
+  const [time, setTime] = useState('');
+  const [date, setDate] = useState('');
+
+  useEffect(() => {
+    const updateDateTime = () => {
+      const now = new Date();
+      setTime(now.toLocaleTimeString('fr-FR', { hour: '2-digit', minute: '2-digit' }));
+      setDate(
+        now.toLocaleDateString('fr-FR', { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' })
+          .charAt(0)
+          .toUpperCase() +
+          now.toLocaleDateString('fr-FR', { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' }).slice(1)
+      );
+    };
+
+    updateDateTime();
+    const intervalId = setInterval(updateDateTime, 1000);
+
+    return () => clearInterval(intervalId);
+  }, []);
+
   return (
     <section className="flex size-full flex-col gap-10 text-white">
-      <h1 className="text-3xl font-bold">
-        Home
-      </h1>
-    </section>
-  )
-}
+      <div className="h-[300px] w-full rounded-[20px] bg-hero bg-cover">
+        <div className="flex h-full flex-col justify-between max-md:px-5 max-md:py-8 lg:p-11">
+          <h2 className="glassmorphism max-w-[270px] rounded py-2 text-center text-base font-normal">
+            Upcoming Meeting at: 10:00 AM
+          </h2>
+          <div className="flex flex-col gap-2">
+            <h1 className="text-4xl font-extrabold lg:text-7xl">{time}</h1>
+            <p className="text-lg font-medium text-sky-1 lg:text-2xl">{date}</p>
+          </div>
+        </div>
+      </div>
 
-export default Home
+      <MeetingTypeList />
+    </section>
+  );
+};
+
+export default Home;
---
+++ components/MeetingTypeList.tsx
@@ -0,0 +1,58 @@
+import Image from 'next/image'
+import React, { useState } from 'react'
+import HomeCard from './HomeCard'
+import { useRouter } from 'next/navigation'
+import MeetingModal from './MeetingModal'
+
+const MeetingTypeList = () => {
+    const router = useRouter();
+    const [meetingState, setMeetingState] = useState<'isScheduleMeeting' | 'isJoiningMeeting' | 'isInstantMeeting' | undefined>();
+
+    const createMeeting = () => {
+        console.log('Creating meeting');
+    }
+  return (
+    <section className="grid grid-cols-1 gap-5 md:grid-cols-2 xl:grid-cols-4">
+        <HomeCard 
+            img='/icons/add-meeting.svg'
+            title='New meeting'
+            description='Start an instant meeting'
+            handleClick={() => setMeetingState('isInstantMeeting')} 
+            className='bg-orange-1'   
+        />
+        <HomeCard 
+            img='/icons/schedule.svg'
+            title='Schedule meeting'
+            description='Plan your meeting'
+            handleClick={() => setMeetingState('isScheduleMeeting')} 
+            className='bg-blue-1'   
+        />
+        <HomeCard 
+            img='/icons/recordings.svg'
+            title='View recordings'
+            description='Check out your recordings'
+            handleClick={() => router.push('/recordings')}
+            className='bg-purple-1'
+        />
+        <HomeCard 
+            img='/icons/join-meeting.svg'
+            title='Join meeting'
+            description='via invitation link'
+            handleClick={() => setMeetingState('isJoiningMeeting')} 
+            className='bg-yellow-1'   
+        />
+
+
+        <MeetingModal
+            isOpen={meetingState === 'isInstantMeeting'}
+            onClose={() => setMeetingState(undefined)}
+            title='Start an instant meeting'
+            className="text-center"
+            buttonText='Start meeting'
+            handleClick={createMeeting}
+        />
+    </section>
+  )
+}
+
+export default MeetingTypeList
